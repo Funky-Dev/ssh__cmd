93332c3e4e7057cbf42c81e65bec5a521d800eb3
funky <funky@humbox.com>
2018-04-24 20:14:53 +0200

    Initial commit

---
+++ app/src/main/java/funky_dev/ssh_cmd/MainActivity.java
@@ -0,0 +1,13 @@
+package funky_dev.ssh_cmd;
+
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
